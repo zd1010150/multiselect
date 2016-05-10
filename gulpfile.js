5d2d9238f4d695bcba275f976617276b1e1fc4ca
kiwizhang.zd <kiwizhang.zd@alibaba-inc.com>
2016-05-10 21:56:28 +0800

    dd

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var babel = require("gulp-babel");
 
 var projects = require("./projects");
 var pro = projects.multiselect;
 
-gulp.task("babel", function () {
-    return gulp.src(pro.jsSrc)
-        .pipe(babel())
-        .pipe(gulp.dest(pro.jsDest))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
+
 gulp.task('html-source', function () {
     gulp.src(pro.htmls)
         .pipe(gulp.dest(pro.htmlDir))
@@ -31,7 +23,6 @@ gulp.task('browserSync', function () {
 });
 
 
-gulp.task('watch', ['browserSync', 'babel', 'html-source'], function () {
-    gulp.watch(pro.jsSrc, ['babel']);
+gulp.task('watch', ['browserSync',  'html-source'], function () {
     gulp.watch(pro.htmls, ['html-source']);
 });
